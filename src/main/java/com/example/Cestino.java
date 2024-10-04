66a8a98043b470eea38416ba669a47ff8e3dbaf0
lai2-24 <lai2-24@itismeucci.com>
2024-10-04 09:04:38 +0200

    errore presente

---
+++ src/main/java/com/example/Cestino.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Cestino {
+    private int contatore;
+
+    public int getContatore() {
+        return contatore;
+    }
+
+    public Cestino(int contatore) {
+        this.contatore = contatore;
+    }
+
+    public void aggiungiMoneta(){
+        contatore = contatore + 1;
+    }
+    
+}
